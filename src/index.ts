26f7f410a361adb1b11bfc651505ec957404250a
unknown <aronilie.code@gmail.com>
2024-04-18 17:43:54 +0200

    Added index

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import "../src/loadEnvironment";
+import connectDB from "../src/database";
+import startServer from "../src/server/startServer";
+
+const http = require("http");
+
+const port = +process.env.PORT || 3100;
+
+const mongoURL = process.env.MONGODB_URL;
+
+(async () => {
+    try {
+        await connectDB(mongoURL);
+        await startServer(port);
+    } catch (error) {
+        process.exit(1);
+    }
+})();
+
