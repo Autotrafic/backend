3aface7d84dccf0ce05f0692a14918e3d3c04a6d
unknown <aronilie.code@gmail.com>
2024-04-18 17:40:19 +0200

    Pure stream busboy without name in folder & file

--- src/server/controllers/filesController.ts
+++ src/server/controllers/filesController.ts
@@ -1,53 +1,39 @@
+import busboy from "busboy";
 import { Request, Response, NextFunction } from "express";
-import {
-    uploadStreamFileToDrive as uploadToGoogleDrive,
-    createFolder,
-} from "../services/googleDrive";
-import { CUSTOMER_FILES_DRIVE_FOLDER_ID } from "../../utils/constants";
 import CustomError from "../../errors/CustomError";
-import { createTextFile, formatDataForTextFile } from "../../utils/file";
+import { createFolder, uploadStreamFileToDrive } from "../services/googleDrive";
+import { CUSTOMER_FILES_DRIVE_FOLDER_ID } from "../../utils/constants";
 
 export const uploadFiles = async (
     req: Request,
     res: Response,
     next: NextFunction
-): Promise<void> => {
-    console.log('inside');
-    const files = req.files as Express.Multer.File[];
-    const { orderData, folderName } = req.body;
+) => {
+    const { body, files } = req;
+    const { folderName } = body;
 
-    if (!files || files.length === 0) {
-        res.status(400).send("No file uploaded.");
-        return;
-    }
+    const bb = busboy({ headers: req.headers });
+    let folderCreated = false;
+    let createdFolderId: string | null = null;
 
-    try {
-        const createdFolderId = await createFolder(
-            folderName,
-            CUSTOMER_FILES_DRIVE_FOLDER_ID
-        );
-
-        const orderDataFile = await createTextFile(
-            formatDataForTextFile(orderData)
-        );
-
-        await uploadToGoogleDrive(
-            orderDataFile as Express.Multer.File,
-            createdFolderId
-        );
-
-        for (const file of files) {
-            await uploadToGoogleDrive(file, createdFolderId);
+    bb.on('file', async (fieldname: string, file: NodeJS.ReadableStream, filename: string, encoding: string, mimetype: string) => {
+        if (!folderCreated) {
+            createdFolderId = await createFolder(req.body.folderName, CUSTOMER_FILES_DRIVE_FOLDER_ID);
+            folderCreated = true;
         }
 
+        try {
+            await uploadStreamFileToDrive({ stream: file, filename, mimetype }, createdFolderId!);
+        } catch (error) {
+            console.error("Error uploading files to Google Drive:", error);
+            next(new CustomError(500, "Failed to upload file to Google Drive.", error.message));
+            return;
+        }
+    });
+
+    bb.on('finish', () => {
         res.status(200).send({ message: "Files uploaded successfully" });
-    } catch (error) {
-        console.error("Error uploading files to Google Drive:", error);
-        const finalError = new CustomError(
-            500,
-            "Failed to upload file to Google Drive.",
-            "Failed to upload file to Google Drive."
-        );
-        next(finalError);
-    }
+    });
+
+    req.pipe(bb);
 };
--- src/server/routes/filesRouter.ts
+++ src/server/routes/filesRouter.ts
@@ -1,48 +1,8 @@
 import express from "express";
-import multer, { FileFilterCallback } from "multer";
-import path from "path";
 import { uploadFiles } from "../controllers/filesController";
 
 const filesRouter = express.Router();
 
-const MAX_SIZE = 50 * 1024 * 1024;
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "uploads/");
-    },
-    filename: function (req, file, cb) {
-        cb(
-            null,
-            file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-        );
-    },
-});
-
-const fileFilter = (
-    req: express.Request,
-    file: Express.Multer.File,
-    cb: FileFilterCallback
-) => {
-    if (
-        file.mimetype.startsWith("image/") ||
-        file.mimetype === "text/plain" ||
-        file.mimetype === "application/pdf" ||
-        file.mimetype ===
-            "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-    ) {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-};
-
-const upload = multer({
-    storage: storage,
-    limits: { fileSize: MAX_SIZE },
-    fileFilter: fileFilter,
-});
-
-filesRouter.post("/upload", upload.any(), uploadFiles);
+filesRouter.post("/upload", uploadFiles);
 
 export default filesRouter;
