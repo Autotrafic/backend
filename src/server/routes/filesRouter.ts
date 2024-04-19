fe33a86ba850d2f0d4098a10b4c82258cbd2ca70
unknown <aronilie.code@gmail.com>
2024-04-19 11:05:58 +0200

    Deleted filters from filesRouter

--- src/server/routes/filesRouter.ts
+++ src/server/routes/filesRouter.ts
@@ -7,41 +7,7 @@ const filesRouter = express.Router();
 
 const MAX_SIZE = 50 * 1024 * 1024;
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "uploads/");
-    },
-    filename: function (req, file, cb) {
-        cb(
-            null,
-            file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-        );
-    },
-});
-
-const fileFilter = (
-    req: express.Request,
-    file: Express.Multer.File,
-    cb: FileFilterCallback
-) => {
-    if (
-        file.mimetype.startsWith("image/") ||
-        file.mimetype === "text/plain" ||
-        file.mimetype === "application/pdf" ||
-        file.mimetype ===
-            "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-    ) {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-};
-
-const upload = multer({
-    storage: storage,
-    limits: { fileSize: MAX_SIZE },
-    fileFilter: fileFilter,
-});
+const upload = multer({ dest: 'uploads/' });
 
 filesRouter.post("/upload", upload.any(), uploadFiles);
 
