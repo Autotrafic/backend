c7a5c3f99ce51dbf34c4ebec2571a97d188c2419
unknown <aronilie.code@gmail.com>
2024-05-02 11:50:31 +0200

    Added ip to whitelist

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,4 +3,10 @@ export const VEHICLE_TYPE = { CAR: 1, MOTORBIKE: 2, CARAVAN: 3 };
 export const CUSTOMER_FILES_DRIVE_FOLDER_ID =
     "1lfiemHUd3-p9ATp29yRw7eEjzhixuB19";
 
-export const WHITELIST_IPS = ["::1", "::ffff:127.0.0.1", '93.176.145.122'];
+export const WHITELIST_IPS = [
+    "::1",
+    "::ffff:127.0.0.1",
+    "93.176.145.122",
+    "79.116.112.200",
+    "79.116.120.191",
+];
