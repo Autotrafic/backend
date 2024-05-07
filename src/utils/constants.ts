d4460b26998e9151a3f1594b4230b0e7da7b3d50
unknown <aronilie.code@gmail.com>
2024-05-07 17:38:26 +0200

    Commented whitelisted ips

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,10 +6,10 @@ export const CUSTOMER_FILES_DRIVE_FOLDER_ID =
 export const WHITELIST_IPS = [
     "::1",
     "::ffff:127.0.0.1",
-    "93.176.145.122", // Wifi Cubelles
-    "79.116.112.200", // Mobil Aron
-    "79.116.120.191", // Mobil Aitor
-    "79.117.90.131", // Mobil Personal Ovi
-    "79.116.111.131", // Mobil T. Ovi
-    "46.222.28.101", // Mobil Meri
+    // "93.176.145.122", // Wifi Cubelles
+    // "79.116.112.200", // Mobil Aron
+    // "79.116.120.191", // Mobil Aitor
+    // "79.117.90.131", // Mobil Personal Ovi
+    // "79.116.111.131", // Mobil T. Ovi
+    // "46.222.28.101", // Mobil Meri
 ];
