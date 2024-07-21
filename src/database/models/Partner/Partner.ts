553a19b20513cc45b966b481f2fed13897280bc4
unknown <aronilie.code@gmail.com>
2024-07-21 19:51:22 +0300

    Added Partner model

---
+++ src/database/models/Partner/Partner.ts
@@ -0,0 +1,12 @@
+export default interface Partner {
+    client: string;
+    fullName: string;
+    iae: number;
+    transferenciaPrice: number;
+    informePrice: number;
+    notificacionPrice: number;
+    batecomPrice: number;
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+}
---
+++ src/database/models/Partner/PrimitivePartner.ts
@@ -0,0 +1,13 @@
+export default interface PrimitivePartner {
+    cliente: string;
+    nombre_completo: string;
+    iae: number;
+    precio_transferencia: number;
+    precio_informe: number;
+    precio_notificacion: number;
+    precio_batecom: number;
+    _id: string;
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+}
