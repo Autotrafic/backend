e315d3eb4838b5416b0a0a33dba622e0c0f8c093
unknown <aronilie.code@gmail.com>
2024-09-06 17:45:50 +0300

    Updated

--- src/server/controllers/filesController.ts
+++ src/server/controllers/filesController.ts
@@ -1,54 +1,47 @@
 import { Request, Response, NextFunction } from "express";
 import {
-    uploadStreamFileToDrive as uploadToGoogleDrive,
-    getOrderFolder,
+  uploadStreamFileToDrive as uploadToGoogleDrive,
+  getOrderFolder,
+  uploadAdditionalInformationFile,
 } from "../services/googleDrive";
 import { CUSTOMER_FILES_DRIVE_FOLDER_ID } from "../../utils/constants";
 import CustomError from "../../errors/CustomError";
-import { createTextFile, formatDataForTextFile } from "../../utils/file";
 
 export default async function uploadFiles(
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<void> {
-    const files = req.files as Express.Multer.File[];
-    const { orderData, folderName } = req.body;
+  const files = req.files as Express.Multer.File[];
+  const { orderData, folderName } = req.body;
 
-    if (!files || files.length === 0) {
-        res.status(400).send(`No file uploaded. Files: ${files}`);
-        return;
+  if (!files || files.length === 0) {
+    res.status(400).send(`No file uploaded. Files: ${files}`);
+    return;
+  }
+
+  try {
+    const orderFolderId = await getOrderFolder(
+      folderName,
+      CUSTOMER_FILES_DRIVE_FOLDER_ID
+    );
+
+    await uploadAdditionalInformationFile(orderData, orderFolderId);
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const file of files) {
+      // eslint-disable-next-line no-await-in-loop
+      await uploadToGoogleDrive(file, orderFolderId);
     }
 
-    try {
-        const orderFolderId = await getOrderFolder(
-            folderName,
-            CUSTOMER_FILES_DRIVE_FOLDER_ID
-        );
-
-        const orderDataFile = await createTextFile(
-            formatDataForTextFile(orderData)
-        );
-
-        await uploadToGoogleDrive(
-            orderDataFile as Express.Multer.File,
-            orderFolderId
-        );
-
-        // eslint-disable-next-line no-restricted-syntax
-        for (const file of files) {
-            // eslint-disable-next-line no-await-in-loop
-            await uploadToGoogleDrive(file, orderFolderId);
-        }
-
-        res.status(200).send({ message: "Files uploaded successfully" });
-    } catch (error) {
-        console.error("Error uploading files to Google Drive:", error);
-        const finalError = new CustomError(
-            500,
-            "Failed to upload files to Google Drive.",
-            `Failed to upload files to Google Drive. \n ${error}`
-        );
-        next(finalError);
-    }
+    res.status(200).send({ message: "Files uploaded successfully" });
+  } catch (error) {
+    console.error("Error uploading files to Google Drive:", error);
+    const finalError = new CustomError(
+      500,
+      "Failed to upload files to Google Drive.",
+      `Failed to upload files to Google Drive. \n ${error}`
+    );
+    next(finalError);
+  }
 }
--- src/server/services/googleDrive.ts
+++ src/server/services/googleDrive.ts
@@ -2,104 +2,148 @@ import { createReadStream, unlink } from "fs";
 import { google, drive_v3 } from "googleapis";
 import "../../loadEnvironment";
 import { getMonthNameInSpanish } from "../../utils/funcs";
+import { createTextFile, formatDataForTextFile } from "../../utils/file";
 
 const key = {
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 };
 
 const jwtClient = new google.auth.JWT(key.client_email, null, key.private_key, [
-    "https://www.googleapis.com/auth/drive",
+  "https://www.googleapis.com/auth/drive",
 ]);
 const drive = google.drive({ version: "v3", auth: jwtClient });
 
 export async function uploadStreamFileToDrive(
-    file: Express.Multer.File,
-    parentFolderId: string
+  file: Express.Multer.File,
+  parentFolderId: string
 ) {
-    const fileMetadata: drive_v3.Params$Resource$Files$Create = {
-        requestBody: {
-            name: `${file.originalname}`,
-            mimeType: file.mimetype,
-            parents: [parentFolderId],
-        },
-        media: {
-            mimeType: file.mimetype,
-            body: createReadStream(file.path),
-        },
-        fields: "id",
-    };
+  const fileMetadata: drive_v3.Params$Resource$Files$Create = {
+    requestBody: {
+      name: `${file.originalname}`,
+      mimeType: file.mimetype,
+      parents: [parentFolderId],
+    },
+    media: {
+      mimeType: file.mimetype,
+      body: createReadStream(file.path),
+    },
+    fields: "id",
+  };
 
-    try {
-        await drive.files.create(fileMetadata);
+  try {
+    await drive.files.create(fileMetadata);
 
-        unlink(file.path, (err) => console.log(err));
-    } catch (error) {
-        console.error("Failed to upload file:", error);
-        throw error;
-    }
+    unlink(file.path, (err) => console.log(err));
+  } catch (error) {
+    console.error("Failed to upload file:", error);
+    throw error;
+  }
 }
 
 async function createFolder(
-    folderName: string,
-    parentFolderId?: string
+  folderName: string,
+  parentFolderId?: string
 ): Promise<string> {
-    const fileMetadata: drive_v3.Params$Resource$Files$Create = {
-        requestBody: {
-            name: folderName,
-            mimeType: "application/vnd.google-apps.folder",
-            parents: parentFolderId ? [parentFolderId] : [],
-        },
-        fields: "id",
-    };
+  const fileMetadata: drive_v3.Params$Resource$Files$Create = {
+    requestBody: {
+      name: folderName,
+      mimeType: "application/vnd.google-apps.folder",
+      parents: parentFolderId ? [parentFolderId] : [],
+    },
+    fields: "id",
+  };
 
-    try {
-        const response = await drive.files.create(fileMetadata);
-        return response.data.id!;
-    } catch (error) {
-        console.error("Failed to create folder:", error);
-        throw error;
-    }
+  try {
+    const response = await drive.files.create(fileMetadata);
+    return response.data.id!;
+  } catch (error) {
+    console.error("Failed to create folder:", error);
+    throw error;
+  }
 }
 
 async function ensureFolderExists(
-    folderName: string,
-    folderToCheck: string
+  folderName: string,
+  folderToCheck: string
 ): Promise<string | null> {
-    try {
-        const query = `'${folderToCheck}' in parents and name = '${folderName}' and mimeType = 'application/vnd.google-apps.folder' and trashed = false`;
-        const response = await drive.files.list({
-            q: query,
-            fields: "files(id, name)",
-            spaces: "drive",
-        });
+  try {
+    const query = `'${folderToCheck}' in parents and name = '${folderName}' and mimeType = 'application/vnd.google-apps.folder' and trashed = false`;
+    const response = await drive.files.list({
+      q: query,
+      fields: "files(id, name)",
+      spaces: "drive",
+    });
 
-        if (response.data.files && response.data.files.length > 0) {
-            return response.data.files[0].id!;
-            // eslint-disable-next-line no-else-return
-        } else {
-            return await createFolder(folderName, folderToCheck);
-        }
-    } catch (error) {
-        console.error("Failed to check or create folder:", error);
-        throw error;
+    if (response.data.files && response.data.files.length > 0) {
+      return response.data.files[0].id!;
+      // eslint-disable-next-line no-else-return
+    } else {
+      return await createFolder(folderName, folderToCheck);
     }
+  } catch (error) {
+    console.error("Failed to check or create folder:", error);
+    throw error;
+  }
 }
 
 export async function getOrderFolder(
-    folderName: string,
-    parentFolderId: string
+  folderName: string,
+  parentFolderId: string
 ): Promise<string> {
-    try {
-        const actualMonthName = getMonthNameInSpanish().toUpperCase();
-        const monthFolderId = await ensureFolderExists(
-            actualMonthName,
-            parentFolderId
-        );
+  try {
+    const actualMonthName = getMonthNameInSpanish().toUpperCase();
+    const monthFolderId = await ensureFolderExists(
+      actualMonthName,
+      parentFolderId
+    );
 
-        return await ensureFolderExists(folderName, monthFolderId);
-    } catch (error) {
-        console.error("Failed to create folder inside the month name:", error);
-        throw error;
+    return await ensureFolderExists(folderName, monthFolderId);
+  } catch (error) {
+    console.error("Failed to create folder inside the month name:", error);
+    throw error;
+  }
+}
+
+export async function checkFileExists(
+  fileName: string,
+  folderId: string
+): Promise<string | null> {
+  try {
+    const query = `'${folderId}' in parents and name = '${fileName}' and trashed = false`;
+    const response = await drive.files.list({
+      q: query,
+      fields: "files(id, name)",
+      spaces: "drive",
+    });
+
+    if (response.data.files && response.data.files.length > 0) {
+      return response.data.files[0].id!;
+    } else {
+      return null;
     }
+  } catch (error) {
+    console.error("Failed to check file existence:", error);
+    throw error;
+  }
+}
+
+export async function uploadAdditionalInformationFile(
+  stringifiedOrderData: string,
+  orderFolderId: string
+) {
+  const isAlreadyUploaded = await checkFileExists(
+    "Información Adicional",
+    orderFolderId
+  );
+  if (isAlreadyUploaded) return;
+
+  const orderDataFile = await createTextFile(
+    formatDataForTextFile(stringifiedOrderData)
+  );
+
+  await uploadStreamFileToDrive(
+    orderDataFile as Express.Multer.File,
+    orderFolderId
+  );
 }
