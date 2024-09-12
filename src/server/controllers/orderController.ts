c35bd0c2af481d2aaf0e8705ccdf783ae7e74bfc
unknown <aronilie.code@gmail.com>
2024-09-12 11:22:06 +0300

    Deleted unnecessary line

--- src/server/controllers/orderController.ts
+++ src/server/controllers/orderController.ts
@@ -1,7 +1,6 @@
 /* eslint-disable guard-for-in */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable consistent-return */
-import "../../interfaces/totalum/pedido";
 import { NextFunction, Request, Response } from "express";
 import CustomError from "../../errors/CustomError";
 import WebOrderModel from "../../database/models/Order/WebOrderSchema";
