c58e0ee910e02f83240b2f311c8679472ea9dc89
unknown <aronilie.code@gmail.com>
2024-09-12 19:17:15 +0300

    Prod

--- src/server/services/stripe.ts
+++ src/server/services/stripe.ts
@@ -3,70 +3,68 @@ import "../../loadEnvironment";
 
 const isProduction = process.env.NODE_ENV === "production";
 
-// const stripeKey = isProduction
-//     ? process.env.STRIPE_SECRET_KEY
-//     : process.env.STRIPE_TEST_SECRET_KEY;
-
-    const stripeKey = process.env.STRIPE_TEST_SECRET_KEY;
+const stripeKey = isProduction
+  ? process.env.STRIPE_SECRET_KEY
+  : process.env.STRIPE_TEST_SECRET_KEY;
 
 const stripe = new Stripe(stripeKey, {});
 
 interface StripeUser {
-    name: string;
-    email: string;
-    phone: string;
-    preferred_locales?: string[];
+  name: string;
+  email: string;
+  phone: string;
+  preferred_locales?: string[];
 }
 
 interface StripeCustomer {
-    fullName: string;
-    phoneNumber: string;
-    email: string;
-    stripeId: string;
+  fullName: string;
+  phoneNumber: string;
+  email: string;
+  stripeId: string;
 }
 
 interface PaymentIntent {
-    paymentMethods: string[];
-    automaticPaymentMethods: { enabled: boolean };
-    amount: number;
-    currency: string;
-    customer: string;
+  paymentMethods: string[];
+  automaticPaymentMethods: { enabled: boolean };
+  amount: number;
+  currency: string;
+  customer: string;
 }
 
 export async function createStripePaymentIntent({
-    automaticPaymentMethods,
-    amount,
-    currency = "eur",
-    customer,
+  automaticPaymentMethods,
+  amount,
+  currency = "eur",
+  customer,
 }: PaymentIntent) {
-    return stripe.paymentIntents.create({
-        // payment_method_types: paymentMethods,
-        automatic_payment_methods: automaticPaymentMethods,
-        amount,
-        currency,
-        customer,
-    });
+  return stripe.paymentIntents.create({
+    // payment_method_types: paymentMethods,
+    automatic_payment_methods: automaticPaymentMethods,
+    amount,
+    currency,
+    customer,
+  });
 }
 
 export async function createStripeCustomer(user: StripeUser) {
-    return stripe.customers.create({
-        name: user.name,
-        email: user.email,
-        phone: user.phone,
-        preferred_locales: ["es"],
-    });
+  return stripe.customers.create({
+    name: user.name,
+    email: user.email,
+    phone: user.phone,
+    preferred_locales: ["es"],
+  });
 }
 
 export async function updateStripeCustomer({
-    fullName,
-    phoneNumber,
-    email,
-    stripeId,
+  fullName,
+  phoneNumber,
+  email,
+  stripeId,
 }: StripeCustomer) {
-    return stripe.customers.update(stripeId, {
-        name: fullName,
-        email,
-        phone: phoneNumber,
-        preferred_locales: ["es"],
-    });
+  return stripe.customers.update(stripeId, {
+    name: fullName,
+    email,
+    phone: phoneNumber,
+    preferred_locales: ["es"],
+  });
 }
