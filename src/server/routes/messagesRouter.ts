682df1aaec81eaa51d97907923dd3b85b028e34d
unknown <aronilie.code@gmail.com>
2024-09-18 13:44:12 +0300

    Updated messages router

--- src/server/routes/messagesRouter.ts
+++ src/server/routes/messagesRouter.ts
@@ -1,10 +1,8 @@
 import express from "express";
-import sendMessage from "../controllers/whatsappController";
 import sendSlackMessage from "../controllers/notifierController";
 
 const messagesRouter = express.Router();
 
-messagesRouter.post("", sendMessage);
 messagesRouter.post("/slack", sendSlackMessage);
 
 export default messagesRouter;
