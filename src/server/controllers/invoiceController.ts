5be37f4c560f75d73051fa9a4d03df8a121b87d4
unknown <aronilie.code@gmail.com>
2024-09-29 16:53:01 +0300

    Updated checks for invoice data

--- src/server/controllers/invoiceController.ts
+++ src/server/controllers/invoiceController.ts
@@ -1,39 +1,29 @@
-import { NextFunction, Request, Response } from "express";
-import { parseOrderFromTotalumToWeb } from "../parsers/order";
-import parseClientFromPrimitive from "../parsers/client";
-import { updateInvoiceNumber } from "../services/invoice";
-import parseInvoiceData from "../parsers/invoice";
-import CustomError from "../../errors/CustomError";
+import { NextFunction, Request, Response } from 'express';
+import { parseOrderFromTotalumToWeb } from '../parsers/order';
+import parseClientFromPrimitive from '../parsers/client';
+import { updateInvoiceNumber } from '../services/invoice';
+import parseInvoiceData from '../parsers/invoice';
+import CustomError from '../../errors/CustomError';
 
-export async function createInvoiceData(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+export async function createInvoiceData(req: Request, res: Response, next: NextFunction) {
   try {
-    const {
-      orderData,
-      clientData,
-      partnerData,
-      currentInvoiceNumber,
-      isForClient,
-    } = req.body;
+    const { orderData, clientData, partnerData, currentInvoiceNumber, isForClient } = req.body;
 
     const order = parseOrderFromTotalumToWeb(orderData);
     const client = parseClientFromPrimitive(partnerData ?? clientData);
 
-    if ((!client && !order.shipmentAddress) || (!client.address && !order.shipmentAddress)) {
+    if (!client) {
+      res.status(400).send(`${order.vehiclePlate} no contiene cliente o socio profesional para generar la factura.`);
+      return;
+    }
+
+    if (!client.address && !order.shipmentAddress) {
       res.status(400).send(`${order.vehiclePlate} no contiene direccion para generar la factura.`);
       return;
     }
 
     const invoiceNumber = updateInvoiceNumber(currentInvoiceNumber);
-    const invoiceData = parseInvoiceData(
-      order,
-      client,
-      invoiceNumber,
-      isForClient
-    );
+    const invoiceData = parseInvoiceData(order, client, invoiceNumber, isForClient);
 
     res.status(200).json(invoiceData);
 
@@ -52,31 +42,21 @@ export async function createInvoiceData(
   }
 }
 
-export async function updateInvoiceData(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+export async function updateInvoiceData(req: Request, res: Response, next: NextFunction) {
   try {
-    const { orderData, clientData, currentInvoiceNumber, isForClient } =
-      req.body;
+    const { orderData, clientData, currentInvoiceNumber, isForClient } = req.body;
 
     const order = parseOrderFromTotalumToWeb(orderData);
     const client = parseClientFromPrimitive(clientData);
 
-    const invoiceData = parseInvoiceData(
-      order,
-      client,
-      currentInvoiceNumber,
-      isForClient
-    );
+    const invoiceData = parseInvoiceData(order, client, currentInvoiceNumber, isForClient);
 
     res.status(200).json(invoiceData);
   } catch (error) {
     console.log(error);
     const finalError = new CustomError(
       400,
-      "Error updating invoice data.",
+      'Error updating invoice data.',
       `Error updating invoice data.
       ${error}.
       
