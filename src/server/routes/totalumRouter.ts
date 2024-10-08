9fa209cf096a5e96cae6f868bac34142a22a3e2e
unknown <aronilie.code@gmail.com>
2024-10-08 16:04:56 +0300

    Added update task route

--- src/server/routes/totalumRouter.ts
+++ src/server/routes/totalumRouter.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getPendingTotalumTasks, toggleTotalumActiveHeader } from '../controllers/totalumController';
+import { getPendingTotalumTasks, toggleTotalumActiveHeader, updateTotalumTask } from '../controllers/totalumController';
 
 const totalumRouter = express.Router();
 
 totalumRouter.post('/active-header', toggleTotalumActiveHeader);
 
 totalumRouter.get('/pending-tasks', getPendingTotalumTasks);
+totalumRouter.post('/update-task', updateTotalumTask);
 
 export default totalumRouter;
