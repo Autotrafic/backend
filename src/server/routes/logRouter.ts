a516921e8c6f268a3d20213a98dd4b02fa146fa2
unknown <aronilie.code@gmail.com>
2024-10-11 16:12:44 +0300

    Removed activity logs

--- src/server/routes/logRouter.ts
+++ src/server/routes/logRouter.ts
@@ -1,26 +1,15 @@
 import express from 'express';
 import {
-  getAllUserLogs,
-  getSessionLogsFromUserId,
-  getActivityLogsFromSessionId,
-  logActivity,
   logWhatsappClick,
   getWhatsappLogs,
   logWebConsult,
 } from '../controllers/loggerController';
-import { checkIPWhitelist } from '../middleweares/checkIPWhitelist';
 
 const logRouter = express.Router();
-
-logRouter.get('/', getAllUserLogs);
 
 logRouter.post('/web-consult', logWebConsult);
 
 logRouter.post('/whatsapp-click', logWhatsappClick);
 logRouter.get('/whatsapp-click', getWhatsappLogs);
-
-logRouter.get('/user/:userId', getSessionLogsFromUserId);
-logRouter.get('/session/:sessionId', getActivityLogsFromSessionId);
-logRouter.post('/register', checkIPWhitelist, logActivity);
 
 export default logRouter;
