4a4653db958cbb02a4114b641d0a782708a4f0dd
unknown <aronilie.code@gmail.com>
2024-10-14 09:33:18 +0300

    Updated shipment configuration

--- src/server/parsers/shipment.ts
+++ src/server/parsers/shipment.ts
@@ -52,7 +52,7 @@ export function createParcelFromShipment(shipment: ParsedTotalumShipment, isTest
     city: shipment.city,
     country: 'ES',
     country_state: null,
-    email: shipment.email,
+    email: shipment.email ?? 'gestoria.autotrafic@gmail.com',
     customs_invoice_nr: '',
     customs_shipment_type: 1,
     parcel_items: [
