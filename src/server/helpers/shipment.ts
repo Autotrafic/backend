58caf60d9e3f101243567b8f918646b18cf43d00
unknown <aronilie.code@gmail.com>
2024-12-12 14:00:50 +0200

    Production of shipments batch version

--- src/server/helpers/shipment.ts
+++ src/server/helpers/shipment.ts
@@ -1,10 +1,9 @@
 import { Response } from 'express';
-import { ExtendedTotalumShipment, TotalumShipment } from '../../interfaces/totalum/envio';
+import { ExtendedTotalumShipment } from '../../interfaces/totalum/envio';
 import { sleep } from '../../utils/funcs';
 import { makeShipment, progressMap, temporaryStorage } from '../handlers/shipment';
 import { mergePdfFromBase64Strings } from '../parsers/file';
 import { getExtendedShipmentById } from '../services/totalum';
-import { PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 
 export function initializeRequestContext(requestId: string, numberOfShipments: number): void {
   if (!progressMap[requestId]) {
@@ -22,41 +21,6 @@ export async function fetchAndCleanShipments(shipmentsId: string[]): Promise<Ext
   return shipments.filter((shipment) => shipment !== undefined);
 }
 
-async function createPdfAsBase64WithDelay(text: string): Promise<string> {
-    // Create a new PDF document
-    const pdfDoc = await PDFDocument.create();
-  
-    // Add a blank page
-    const page = pdfDoc.addPage([600, 400]);
-  
-    // Embed the standard font
-    const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
-  
-    // Define font size and color
-    const fontSize = 24;
-    const textColor = rgb(0, 0, 0);
-  
-    // Add text to the page
-    page.drawText(text, {
-      x: 50,
-      y: 300,
-      size: fontSize,
-      font: font,
-      color: textColor,
-    });
-  
-    // Serialize the PDF document to bytes
-    const pdfBytes = await pdfDoc.save();
-  
-    // Introduce a 3-second delay
-    await sleep(3000);
-  
-    // Convert the bytes to a Base64 string
-    const labelBase64 = Buffer.from(pdfBytes).toString('base64');
-  
-    return labelBase64;
-  }
-
 export async function processShipmentsBatch(
   requestId: string,
   shipments: ExtendedTotalumShipment[],
@@ -65,8 +29,7 @@ export async function processShipmentsBatch(
 ): Promise<void> {
   for (const shipment of shipments) {
     try {
-    //   const label = await makeShipment({ totalumShipment: shipment, isTest });
-    const label = await createPdfAsBase64WithDelay('Here and now');
+    const label = await makeShipment({ totalumShipment: shipment, isTest });
       temporaryStorage[requestId].push(label);
 
       progressMap[requestId].processedCount = (progressMap[requestId].processedCount || 0) + 1;
